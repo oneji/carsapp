24d0562b2b959b249d500ebc34f223166923c202
Timur <timich1995@gmail.com>
2018-06-14 14:24:28 +0500

    1323

--- clientfront/deploy.sh
+++ clientfront/deploy.sh
@@ -1,23 +1,8 @@
-#!/bin/bash
-
-HOST=ftp://cars.55soft.net  #This is the FTP servers host or IP address.
-USER=projectEHD55familySWcars             #This is the FTP user that has access to the server.
-PASS=YW4C%S%4s          #This is the password for the FTP user.
-
-# Call 1. Uses the ftp command with the -inv switches. 
-#-i turns off interactive prompting. 
-#-n Restrains FTP from attempting the auto-login feature. 
-#-v enables verbose and progress. 
-
-ftp -inv $HOST << EOF
-
-user $USER $PASS
-
-cd /path/to/file
-
-put dist/index.html
-
-# End FTP Connection
+read -s -p "Enter Password: " mypassword
+ncftp <<EOF
+open -u projectEHD55familySWcars -p $mypassword ftp://cars.55soft.net
+cd /
+lcd /dist
+put -R *
 bye
-
 EOF
