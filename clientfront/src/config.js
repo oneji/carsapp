b0b6bf0058fa08df4d2a6a4346e9845ddd1b1fd7
Timur <timich1995@gmail.com>
2018-10-25 20:22:12 +0800

    Get rid of slash in api url.

--- clientfront/src/config.js
+++ clientfront/src/config.js
@@ -1,4 +1,4 @@
 export default {
-    apiURL: process.env.NODE_ENV === 'production' ? '//api.cars.55soft.net/api/' : '//55cars.loc/api/',
+    apiURL: process.env.NODE_ENV === 'production' ? '//api.cars.55soft.net/api' : '//55cars.loc/api',
     assetsURL: process.env.NODE_ENV === 'production' ? '//api.cars.55soft.net' : '//55cars.loc'
 }
